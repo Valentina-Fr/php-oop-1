0a2af7ed86517eb4707e6c71db46c6a609e04ad5
Valentina-Fr <v_frasca@virgilio.it>
2021-10-05 17:15:53 +0200

    Create two Movie obj

--- index.php
+++ index.php
@@ -11,4 +11,22 @@ class Movie {
         $this->director = $_director;
         $this->year = $_year;
     }
+
+    public function setLanguage($lang) {
+        if($lang == "ita") {
+            $this->language = "Italian";
+        } elseif ($lang == "eng") {
+            $this->language = "English";
+        } else {
+            $this->language = "unknown";
+        }
+    }
 }
+
+$pinocchio = new Movie("Pinocchio", "Matteo Garrone", 2019);
+$pinocchio->setLanguage("ita");
+var_dump($pinocchio);
+
+$titanic = new Movie("Titanic", "James Cameron", 1997);
+$titanic->setLanguage("eng");
+var_dump($titanic);
