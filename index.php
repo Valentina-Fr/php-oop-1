ce35c0d0637242dd404bc42825ddafd407f60740
Valentina-Fr <v_frasca@virgilio.it>
2021-10-05 16:42:57 +0200

    Add construct

--- index.php
+++ index.php
@@ -5,4 +5,10 @@ class Movie {
     public $year;
     public $length; 
     public $language;
+
+    function __construct($_title, $_director, $_year) {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->year = $_year;
+    }
 }
